cefb105a9e6d4271441b406ebf6695ced99b2830
Oguz <oguzcanyucetr@gmail.com>
2022-05-23 07:45:10 +0300

    vuex imported and structured

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,10 +1,28 @@
 <template>
   <section>FILTER</section>
   <section>
-      <div class="controls">
-          <button>Refresh</button>
-          <router-link to="/register">Register as a coach</router-link>
-      </div>
-      <ul>LIST OF Coaches</ul>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as a coach</router-link>
+    </div>
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
+    </ul>
+    <h3 v-else>No coach found.</h3>
   </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
+</script>
