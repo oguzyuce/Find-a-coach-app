33798dca7b709fec578b008847020ba1f5ce7872
Oguz <oguzcanyucetr@gmail.com>
2022-05-24 22:10:14 +0300

    base components created.

---
+++ src/components/ui/BaseCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(151, 175, 16, 0.911);
+  padding: 1rem;
+  margin: 2rem auto;
+  max-width: 40rem;
+  color: rgba(151, 175, 16, 0.911);
+}
+</style>
