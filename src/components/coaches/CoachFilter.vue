da64fc6fa7a73f03bc3b0ca1769e46445de9629a
Oguz <oguzcanyucetr@gmail.com>
2022-05-26 20:03:39 +0300

    coachdetail component modified

---
+++ src/components/coaches/CoachFilter.vue
@@ -0,0 +1,17 @@
+<template>
+  <base-card>
+    <h2>Filter your coach</h2>
+    <span class="filter-option">
+      <input type="checkbox" id="frontend" checked />
+      <label for="frontend">Frontend</label>
+    </span>
+    <span class="filter-option">
+      <input type="checkbox" id="backend" checked />
+      <label for="backend">Backend</label>
+    </span>
+    <span class="filter-option">
+      <input type="checkbox" id="career" checked />
+      <label for="career">Career</label>
+    </span>
+  </base-card>
+</template>
--- src/components/ui/BaseCard.vue
+++ src/components/ui/BaseCard.vue
@@ -7,10 +7,9 @@
 <style scoped>
 .card {
   border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(151, 175, 16, 0.911);
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 1rem;
   margin: 2rem auto;
   max-width: 40rem;
-  color: rgba(151, 175, 16, 0.911);
 }
 </style>
